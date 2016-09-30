0c79318639f0278e11ee61a11ca5e0506066a580
Amine Tayaa <amine.tayaa@gmail.com>
2016-09-30 16:12:13 +0200

    base de donnees

--- db.sql
+++ db.sql
@@ -1,31 +1,21 @@
 
 CREATE TABLE IF NOT EXISTS `membres` (
-  `id_membre` int(10) unsigned NOT NULL AUTO_INCREMENT,
-  `login` varchar(50) NOT NULL,
-  `pass` varchar(50) NOT NULL,
+  `id_membre` INTEGER PRIMARY KEY,
+  `login` TEXT NOT NULL UNIQUE,
+  `pass` TEXT NOT NULL,
   `actif` tinyint(1) NOT NULL,
-  `estAdmin` tinyint(1) NOT NULL,
-  PRIMARY KEY (`id_membre`),
-  UNIQUE KEY `login` (`login`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+  `estAdmin` tinyint(1) NOT NULL
+);
 
 CREATE TABLE `messages` (
-  `id_message` int(11) NOT NULL AUTO_INCREMENT,
-  `sujet` varchar(200) NOT NULL,
-  `corps` text NOT NULL,
-  `id_expediteur` int(11) NOT NULL,
-  `id_destinataire` int(11) NOT NULL,
-  PRIMARY KEY (`id_message`),
+  `id_message` INTEGER NOT NULL PRIMARY KEY,
+  `sujet` TEXT NOT NULL,
+  `corps` TEXT NOT NULL,
+  `id_expediteur` INTEGER NOT NULL,
+  `id_destinataire` INTEGER NOT NULL,
   FOREIGN KEY(`id_expediteur`) REFERENCES `membres`(`id_membre`),
   FOREIGN KEY(`id_destinataire`) REFERENCES `membres`(`id_membre`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 ;
+);
 
-
-
-
-INSERT INTO INSERT INTO `users` (`id_user`, `login`, `pass`, `actif`, `estAdmin`) VALUES
+INSERT INTO `membres` (`id_membre`, `login`, `pass`, `actif`, `estAdmin`) VALUES
 (1, 'admin', 'administrateur', 1, 1);
-
-
-
-
